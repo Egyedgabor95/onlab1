4716a6997707319d6ac81190093721cb8ea26d24
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-06 16:38:52 +0200

    add CoachEntity

---
+++ src/main/java/com/example/onlab_egyed_gabor/data/entities/CoachEntity.java
@@ -0,0 +1,12 @@
+package com.example.onlab_egyed_gabor.data.entities;
+
+public class CoachEntity {
+
+    private int id;
+
+    private String name;
+    private int experience;
+
+    private int value;
+    private int salary;
+}
