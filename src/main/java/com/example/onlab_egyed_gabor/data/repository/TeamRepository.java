f373feee570ebee8dc71e67b22f90627bb023cb6
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-06 18:04:44 +0200

    bugfix

--- src/main/java/com/example/onlab_egyed_gabor/data/repository/TeamRepository.java
+++ src/main/java/com/example/onlab_egyed_gabor/data/repository/TeamRepository.java
@@ -1,8 +1,9 @@
 package com.example.onlab_egyed_gabor.data.repository;
 
+import com.example.onlab_egyed_gabor.data.entities.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository extends JpaRepository<TeamRepository, Integer> {
+public interface TeamRepository extends JpaRepository<TeamEntity, Integer> {
 }
