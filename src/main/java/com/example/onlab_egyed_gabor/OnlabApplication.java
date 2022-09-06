0f6425f375450a2f2ce4bfa4e4988f10ab682713
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-06 15:49:03 +0200

    init

--- src/main/java/com/example/onlab_egyed_gabor/OnlabApplication.java
+++ src/main/java/com/example/onlab_egyed_gabor/OnlabApplication.java
@@ -1,13 +1,13 @@
-package com.example.spring_basic;
+package com.example.onlab_egyed_gabor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBasicApplication {
+public class OnlabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBasicApplication.class, args);
+		SpringApplication.run(OnlabApplication.class, args);
 	}
 
 }
