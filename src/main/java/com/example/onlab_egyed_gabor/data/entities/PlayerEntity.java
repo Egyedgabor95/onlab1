51d74bf7491bf6cad87580f127c4b52295547f35
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-06 16:22:13 +0200

    add PlayerEntity

---
+++ src/main/java/com/example/onlab_egyed_gabor/data/entities/PlayerEntity.java
@@ -0,0 +1,21 @@
+package com.example.onlab_egyed_gabor.data.entities;
+
+import com.example.onlab_egyed_gabor.data.enums.Position;
+
+public class PlayerEntity {
+
+    private int id;
+
+    private String name;
+    private Position position;
+    private String teamName;
+    private int playerNumber;
+
+    private double value;
+    private double salary;
+    private int contractDeadline;
+
+    private int skillPoint;
+    private int health;
+    private int redCardCounter;
+}
