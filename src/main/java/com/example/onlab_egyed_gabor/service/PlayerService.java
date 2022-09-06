fc4398792aef78d1f796cc4adf662dc775493277
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-06 18:06:49 +0200

    create service templates

---
+++ src/main/java/com/example/onlab_egyed_gabor/service/PlayerService.java
@@ -0,0 +1,7 @@
+package com.example.onlab_egyed_gabor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService {
+}
---
+++ src/main/java/com/example/onlab_egyed_gabor/service/TeamService.java
@@ -0,0 +1,36 @@
+package com.example.onlab_egyed_gabor.service;
+
+import com.example.onlab_egyed_gabor.controller.dto.TeamDTO;
+import com.example.onlab_egyed_gabor.data.entities.TeamEntity;
+import com.example.onlab_egyed_gabor.data.mapper.TeamMapper;
+import com.example.onlab_egyed_gabor.data.repository.TeamRepository;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Getter
+public class TeamService {
+    @Autowired
+    private TeamRepository teamRepository;
+
+    private TeamMapper teamMapper;
+
+    public void create(TeamDTO teamDTO) {
+        getTeamRepository().save(getTeamMapper().teamDTOToTeamEntity(teamDTO));
+    }
+
+    public List<TeamEntity> read() {
+        return getTeamRepository().findAll();
+    }
+
+    public void update(TeamDTO newTeam) {
+        //TODO
+    }
+
+    public void delete(TeamDTO teamDTO) {
+        getTeamRepository().deleteById(getTeamMapper().teamDTOToTeamEntity(teamDTO).getId());
+    }
+}
