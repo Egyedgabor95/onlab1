a90adfd4ab606a1ede011414b0d8db8a33eeba87
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-09 16:13:58 +0200

    create coach api and getters for it

--- src/main/java/com/example/onlab_egyed_gabor/controller/dto/CoachDTO.java
+++ src/main/java/com/example/onlab_egyed_gabor/controller/dto/CoachDTO.java
@@ -1,5 +1,10 @@
 package com.example.onlab_egyed_gabor.controller.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CoachDTO {
     private int id;
     private String name;
---
+++ src/main/java/com/example/onlab_egyed_gabor/controller/rest/CoachRestController.java
@@ -0,0 +1,41 @@
+package com.example.onlab_egyed_gabor.controller.rest;
+
+import com.example.onlab_egyed_gabor.controller.dto.CoachDTO;
+import com.example.onlab_egyed_gabor.data.entities.CoachEntity;
+import com.example.onlab_egyed_gabor.service.implementation.CoachService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/coach")
+public class CoachRestController {
+
+    @Autowired
+    private CoachService coachService;
+
+    @PostMapping
+    public List<CoachEntity> create(@RequestBody CoachDTO coachDTO) {
+        coachService.create(coachDTO);
+        return coachService.read();
+    }
+
+    @GetMapping
+    public List<CoachEntity> read() {
+        return coachService.read();
+    }
+
+    @PutMapping("/{id}")
+    public List<CoachEntity> update(@PathVariable int id, @RequestBody CoachDTO coachDTO) {
+        coachService.update(id, coachDTO);
+        return coachService.read();
+    }
+
+    @DeleteMapping("/{id}")
+    public List<CoachEntity> delete(@PathVariable int id) {
+        coachService.delete(id);
+        return coachService.read();
+    }
+}
