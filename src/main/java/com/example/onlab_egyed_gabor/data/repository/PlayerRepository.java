37766171f189dc6d393705312e679ba77068b8f4
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-09-13 16:57:03 +0200

    bugfix

--- src/main/java/com/example/onlab_egyed_gabor/data/repository/PlayerRepository.java
+++ src/main/java/com/example/onlab_egyed_gabor/data/repository/PlayerRepository.java
@@ -1,8 +1,9 @@
 package com.example.onlab_egyed_gabor.data.repository;
 
+import com.example.onlab_egyed_gabor.data.entities.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository extends JpaRepository<PlayerRepository, Integer> {
+public interface PlayerRepository extends JpaRepository<PlayerEntity, Integer> {
 }
