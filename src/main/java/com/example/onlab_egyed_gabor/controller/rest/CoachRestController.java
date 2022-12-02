bfb7fa2d1b038ccd5be18fb128c31b30fc4f5468
Egyed Gábor <gabor.egyed@nitrowise.com>
2022-12-02 19:49:41 +0100

    add log + ResponseEntity to coach

--- src/main/java/com/example/onlab_egyed_gabor/controller/rest/CoachRestController.java
+++ src/main/java/com/example/onlab_egyed_gabor/controller/rest/CoachRestController.java
@@ -1,13 +1,17 @@
 package com.example.onlab_egyed_gabor.controller.rest;
 
 import com.example.onlab_egyed_gabor.controller.dto.CoachDTO;
+import com.example.onlab_egyed_gabor.data.entities.CoachEntity;
 import com.example.onlab_egyed_gabor.service.implementation.CoachService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/coach")
 public class CoachRestController {
@@ -16,25 +20,34 @@ public class CoachRestController {
     private CoachService coachService;
 
     @PostMapping
-    public List<CoachDTO> create(@RequestBody CoachDTO coachDTO) {
-        coachService.create(coachDTO);
-        return coachService.read();
+    public ResponseEntity<CoachEntity> create(@RequestBody CoachDTO coachDTO) {
+        log.debug("CoachRestController : create");
+        return new ResponseEntity<>(coachService.create(coachDTO), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<CoachDTO> read() {
-        return coachService.read();
+    public ResponseEntity<List<CoachDTO>> read() {
+        log.debug("CoachRestController : read");
+        return ResponseEntity.ok(coachService.read());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CoachDTO> readOne(@PathVariable Long id) {
+        log.debug("CoachRestController : readOne");
+        return ResponseEntity.ok(coachService.findById(id));
     }
 
     @PutMapping("/{id}")
-    public List<CoachDTO> update(@PathVariable int id, @RequestBody CoachDTO coachDTO) {
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody CoachDTO coachDTO) {
+        log.debug("CoachRestController : update");
         coachService.update(id, coachDTO);
-        return coachService.read();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{id}")
-    public List<CoachDTO> delete(@PathVariable int id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.debug("CoachRestController : delete");
         coachService.delete(id);
-        return coachService.read();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/onlab_egyed_gabor/service/implementation/CoachService.java
+++ src/main/java/com/example/onlab_egyed_gabor/service/implementation/CoachService.java
@@ -5,12 +5,14 @@ import com.example.onlab_egyed_gabor.data.entities.CoachEntity;
 import com.example.onlab_egyed_gabor.data.mapper.CoachMapper;
 import com.example.onlab_egyed_gabor.data.repository.CoachRepository;
 import com.example.onlab_egyed_gabor.service.interfaces.ICoachService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Transactional
 @Service
 public class CoachService implements ICoachService {
@@ -22,30 +24,36 @@ public class CoachService implements ICoachService {
 
     @Override
     public CoachEntity create(CoachDTO coachDTO) {
+        log.debug("CoachService : create = {}", coachDTO);
         return coachRepository.save(coachMapper.coachDTOToCoachEntity(coachDTO));
     }
 
     @Override
     public List<CoachDTO> read() {
+        log.debug("CoachService : read");
         return coachMapper.coachEntityListToCoachDTOList(coachRepository.findAll());
     }
 
     @Override
     public void update(Long targetId, CoachDTO source) {
+        log.debug("CoachService : update, targetId = {}, source = {}", targetId, source);
         coachMapper.update(findEntityById(targetId), source);
     }
 
     @Override
     public void delete(Long id) {
+        log.debug("CoachService : delete, id = {}", id);
         coachRepository.delete(findEntityById(id));
     }
 
     @Override
     public CoachDTO findById(Long id) {
+        log.debug("CoachService : findById, id = {}", id);
         return coachMapper.coachEntityToCoachDTO(coachRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find Coach with this id")));
     }
 
     private CoachEntity findEntityById(Long id) {
+        log.debug("CoachService : findEntityById, id = {}", id);
         return coachRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find Coach with this id"));
     }
 }
